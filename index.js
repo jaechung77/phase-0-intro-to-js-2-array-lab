3138d93d53b7684d9122929a6f0a4e1c1720f2a2
jaechung77 <jaechung77@gmail.com>
2021-05-06 01:09:45 +1000

    Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+} 
+
+function appendCat(name){
+    return[...cats, name];
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function prependCat(name){
+    return[name, ...cats];
+}
+
+function removeLastCat(){
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+
+function removeFirstCat(){
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats;
+}
